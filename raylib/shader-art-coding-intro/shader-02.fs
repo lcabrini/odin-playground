7493ae690bab08b7bad3e76fc8481cdaab6b3bde
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2025-07-24 19:05:02 +0000

    Add horizontal graient shader

---
+++ raylib/shader-art-coding-intro/shader-02.fs
@@ -0,0 +1,16 @@
+#version 330
+
+in vec2 fragTexCoord;
+in vec4 fragColor;
+
+out vec4 finalColor;
+
+uniform float renderWidth;
+uniform float renderHeight;
+
+void main() {
+    vec2 uv = fragTexCoord*2.0 - 1.0;
+    uv.x *= renderWidth/renderHeight;
+
+    finalColor = vec4(uv.x, 0.0, 0.0, 1.0);
+}
