9ca4ca5294e566f34c2704f637dbdb802421a78d
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2025-07-24 21:56:32 +0000

    Add pattern 3 shader

--- raylib/shader-art-coding-intro/main.odin
+++ raylib/shader-art-coding-intro/main.odin
@@ -39,6 +39,7 @@ Select the shader you want to test:
 18. Spatial repetition of animated rings
 19. Pattern 1
 20. Pattern 2
+21. Pattern 3
 `
 
 readint :: proc(prompt: string, min, max: int) -> int {
---
+++ raylib/shader-art-coding-intro/shader-21.fs
@@ -0,0 +1,39 @@
+#version 330
+
+in vec2 fragTexCoord;
+in vec4 fragCoord;
+
+out vec4 fragColor;
+
+uniform float renderWidth;
+uniform float renderHeight;
+uniform float time;
+
+vec3 palette(float t) {
+    vec3 a = vec3(0.5, 0.5, 0.5);
+    vec3 b = vec3(0.5, 0.5, 0.5);
+    vec3 c = vec3(1.0, 1.0, 1.0);
+    vec3 d = vec3(0.263, 0.416, 0.557);
+
+    return a + b*cos(6.28318*(c*t*d));
+}
+
+void main() {
+    vec2 uv = vec2(fragTexCoord.x, 1.0-fragTexCoord.y) * 2.0 - 1.0;
+    uv.x *= renderWidth/renderHeight;
+    vec2 uv0 = uv;
+    vec3 finalColor = vec3(0.0);
+
+    for (float i = 0.0; i < 3.0; i++) {
+        uv = fract(uv * 1.5) - 0.5;
+
+        float d = length(uv) * exp(-length(uv0));
+        vec3 col = palette(length(uv0) + time*0.4);
+        d = sin(d*8.0 + time)/8.0;
+        d = abs(d);
+        d = 0.02 / d;
+
+        finalColor += col * d;
+    }
+    fragColor = vec4(finalColor, 1.0);
+}
